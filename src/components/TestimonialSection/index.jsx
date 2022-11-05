ec343b13af413632c39ad28ece72d92ddccfd41f
ultroneous <hello@ultroneous.com>
2022-11-05 12:47:25 +0530

    shot chnages

--- src/components/TestimonialSection/index.jsx
+++ src/components/TestimonialSection/index.jsx
@@ -1,24 +1,38 @@
 import React from "react";
+import CountUp from "react-countup";
 
 const Testimonial = () => {
+  const testimonialCount = [
+    {
+      count: 12,
+      text: "Years Experience",
+    },
+    {
+      count: 200,
+      text: "Exercise Equipment",
+    },
+    {
+      count: 1000,
+      text: "Happy Customers",
+    },
+    {
+      count: 20,
+      text: "Expert Trainers",
+    },
+  ];
+
+
   return (
     <div className="bg-testimonial flex justify-between pl-32 pr-32 pt-11 pb-10">
-      <div className="text-center">
-        <h2 className="text-5xl font-bold">12 +</h2>
-        <h2>Years Experience</h2>
-      </div>
-      <div className="text-center">
-        <h2 className="text-5xl font-bold">200 +</h2>
-        <h2>Exercise Equipment</h2>
-      </div>
-      <div className="text-center">
-        <h2 className="text-5xl font-bold">1000 +</h2>
-        <h2>Happy Customers</h2>
-      </div>
-      <div className="text-center">
-        <h2 className="text-5xl font-bold">20 +</h2>
-        <h2>Expert Trainers</h2>
-      </div>
+      {testimonialCount.map((testimoni, index) => {
+        return (
+          <div className="text-center" key={index}>
+            
+              <h2 className="text-5xl font-bold"><CountUp start={0} end={testimoni.count} /> +</h2>
+              <h2>{testimoni.text}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
